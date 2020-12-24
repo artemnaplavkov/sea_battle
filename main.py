b69a2c7f24436fc3268213c8e400ab99d63453c8
Artem Naplavkov <artem.naplavkov@gmail.com>
2020-12-24 17:52:46 +0300

    random_ship

--- main.py
+++ main.py
@@ -29,12 +29,38 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
+    def is_out(self, x, y):
+        return x < 0 or x >= self.width or y < 0 or y >= self.height
+
     def get_cell(self, mouse_pos):
         cell_x = (mouse_pos[0] - self.left) // self.cell_size
         cell_y = (mouse_pos[1] - self.top) // self.cell_size
-        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
+        if self.is_out(cell_x, cell_y):
             return None
         return cell_x, cell_y
+
+    def is_free(self, x, y):
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if self.is_out(x + dx, y + dy):
+                    continue
+                if self.board[y + dy][x + dx] != water:
+                    return False
+        return True
+
+    def random_ship(self, decks):
+        result = []
+        x = random.randint(0, self.width - 1)
+        y = random.randint(0, self.height-1)
+        direction = random.randint(0, 1)
+        dx, dy = 0, 0
+        if direction == 0:
+            dx = 1
+        else:
+            dy = 1
+        for deck in range(decks):
+            result.append((x + dx * deck, y + dy * deck))
+        return result
 
     def render(self):
         radius = self.cell_size // 2
@@ -47,6 +73,8 @@ class Board:
                 pygame.draw.rect(screen, pygame.Color('white'), rect, 2)
                 
 player = Board()
+ship = player.random_ship(3)
+print(ship)
 enemy = Board()
 player.set_view(20, 20, 30)
 enemy.set_view(340, 20, 30)
